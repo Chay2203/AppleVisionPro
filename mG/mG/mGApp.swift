4fa242c3ecda88af8f67c637cb0c127816f4c1da
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2024-02-27 09:42:42 +0530

    Add files via upload

---
+++ mG/mG/mGApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct mGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
